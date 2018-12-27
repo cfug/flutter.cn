0c76fe35ee4323d3c83d314a513f9af6187daed7
Zhicheng WANG <zcwang@thoughtworks.com>
2018-12-27 08:26:43 +0800

    fix: 忽略 A 标签 —— 点击它不再展开英文

--- tool/translator/assets/js/translator.js
+++ tool/translator/assets/js/translator.js
@@ -1,6 +1,10 @@
 document.addEventListener('click', function (event) {
   /** @type Element */
   var result = event.target;
+  // 忽略 A 标签
+  if (result.tagName === 'A') {
+    return;
+  }
   if (result.hasAttribute('translation-result')) {
     var origin = result.nextElementSibling;
     if (origin.getAttribute('translation-origin') === 'off') {

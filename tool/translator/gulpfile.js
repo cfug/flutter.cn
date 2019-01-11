c0a5a6500cb1fa545c4cb83373b0849277defcb5
Zhicheng WANG <zcwang@thoughtworks.com>
2019-01-11 21:01:25 +0800

    fix: 对被墙的资源进行本地化 fix: 关闭 integrity 校验

--- tool/translator/gulpfile.js
+++ tool/translator/gulpfile.js
@@ -37,9 +37,12 @@ gulp.task('postprocess', ['prepare-assets'], () => {
       // 处理被墙的 CDN
       .pipe(replace(/https:\/\/fonts.googleapis.com\/icon\?family=Material\+Icons/gim, '/assets/css/material-icons.css'))
       .pipe(replace(/https:\/\/fonts.googleapis.com\/css\?family=Source\+Code\+Pro%7CRoboto:500,400italic,300,400/gim, '/assets/css/material-roboto-italic.css'))
+      .pipe(replace(/https:\/\/fonts.googleapis.com\/css\?family=Google\+Sans:400,500\|Roboto:300,400,500\|Roboto\+Mono:400,700\|Material\+Icons/gim, '/assets/css/material-roboto.css'))
       .pipe(replace(/https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/jquery\/3.2.1\/jquery.min.js/gim, '/assets/js/jquery.min.js'))
+      .pipe(replace(/https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/clipboard.js\/2.0.0\/clipboard.min.js/gim, '/assets/js/clipboard.min.js'))
       .pipe(replace(/https:\/\/maxcdn.bootstrapcdn.com\/bootstrap\/3.3.4\/js\/bootstrap.min.js/gim, '/assets/js/bootstrap.min.js'))
       .pipe(replace(/https:\/\/maxcdn.bootstrapcdn.com\/font-awesome\/4.5.0\/css\/font-awesome.min.css/gim, '/assets/css/font-awesome.min.css'))
       .pipe(replace(/\/\/survey.g.doubleclick.net\/async_survey\?site=at3ul57xpub2vk3oxt2ytw365i/gim, '/assets/js/async_survey.js'))
+      .pipe(replace(/integrity=".*?"/gim, ''))
       .pipe(gulp.dest('_site/'));
 });

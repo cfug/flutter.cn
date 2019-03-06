8867a92a2567040bcdc59e8359afbacfd82a5075
Zhicheng WANG <zcwang@thoughtworks.com>
2019-03-07 00:10:49 +0800

    chore: 尝试改用新的对照翻译模块

--- tool/translator/build.sh
+++ tool/translator/build.sh
@@ -3,20 +3,6 @@
 set -x
 set -e
 
-cd `dirname $0`
-
-npm install
-
-npm run preprocess
-
-cd -
-
 bundle install
 
 bundle exec jekyll build
-
-cd `dirname $0`
-
-npm run postprocess
-
-cd -
--- tool/translator/deploy-cn.sh
+++ tool/translator/deploy-cn.sh
@@ -8,30 +8,23 @@ commitMessage=$(git log --oneline -n 1)
 
 bundle install
 
-cd `dirname $0`
-
-gulp preprocess
-
-cd -
-
 bundle exec jekyll build
 
-cd `dirname $0`
+cp -r tool/translator/assets/*  _site/assets/
 
-npm install
+npm i -g @awesome-fe/translate
 
-gulp postprocess
+nt inject '_site/**/*.html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m /Users/twer/dev/sdk/flutter-docs-cn/tool/translator/url-map.json -t /Users/twer/dev/sdk/flutter-docs-cn/tool/translator/text-map.json
 
-if [[ ! -d "/tmp/flutter-docs-cn" ]]
-then
-    git clone git@github.com:cfug/flutter.cn-prebuilt.git /tmp/flutter-docs-cn
-else
-    rm -rf /tmp/flutter-docs-cn/
-    git clone git@github.com:cfug/flutter.cn-prebuilt.git /tmp/flutter-docs-cn
-fi
+nt mark '_site/**/*.html'
+
+nt inject '_site/**/*.svg' -t /Users/twer/dev/sdk/flutter-docs-cn/tool/translator/text-map.json
+
+rm -rf /tmp/flutter-docs-cn/
+
+git clone https://asnowwolf:${GITHUB_ACCESS_TOKEN}@github.com/cfug/flutter.cn-prebuilt.git /tmp/flutter-docs-cn
 
 cp -r _site/* /tmp/flutter-docs-cn/
-cp CNAME /tmp/flutter-docs-cn/
 
 cd /tmp/flutter-docs-cn
 
@@ -42,5 +35,3 @@ git commit --allow-empty -am "${commitMessage}"
 git push -u -f origin master
 
 cd -
-
-rm -rf /tmp/flutter-docs-cn/index.html && rm -rf /tmp/flutter-docs-cn/robots.txt && rm -rf /tmp/flutter-docs-cn/CNAME

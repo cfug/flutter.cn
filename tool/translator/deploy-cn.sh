55b66e8541a51587a19ff76e025276b288702bb8
Luke Cheng <chenglu@users.noreply.github.com>
2020-06-06 17:40:25 +0800

    Deploy repo changes.

--- tool/translator/deploy-cn.sh
+++ tool/translator/deploy-cn.sh
@@ -6,16 +6,20 @@ set -e
 commitSha=$(git rev-parse --short HEAD)
 commitMessage=$(git log --oneline -n 1)
 
-rm -rf /tmp/flutter.cn-prebuilt/ || true
+rm -rf /tmp/site-flutter.cn/ || true
 
-git clone https://cfug-deploy:${CFUG_DEPLOY_KEY}@github.com/cfug-deploy/flutter.cn-prebuilt.git /tmp/flutter.cn-prebuilt
+git clone https://chenglu:${CHENGLU_DEPLOY_KEY}@github.com/chenglu/site-flutter.cn /tmp/site-flutter.cn
 
-cp -r _site/* /tmp/flutter.cn-prebuilt/
+cp -r _site/* /tmp/site-flutter.cn/
 
-cd /tmp/flutter.cn-prebuilt
+cd /tmp/site-flutter.cn
 
 git init
 git add .
+
+git config --global user.name "travis-ci deploy"
+git config --global user.email "cfug-dev@googlegroups.com"
+
 git commit --allow-empty -am "${commitMessage}"
 
 git push -u -f origin master

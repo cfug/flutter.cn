3363a3baf738bf05a0f89926ae93674831d6f28c
Luke Cheng <2258420+chenglu@users.noreply.github.com>
2020-11-10 21:16:53 +0800

    Memory fixes for nt mark.

--- tool/translator/build.sh
+++ tool/translator/build.sh
@@ -19,7 +19,7 @@ npx gulp mark-side-toc
 
 npx nt inject '../../_site/**/*.html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json
 
-npx nt mark '../../_site/**/*.html'
+npx --node-arg --max-old-space-size=2048 nt mark '../../_site/**/*.html'
 
 npx gulp remove-space
 

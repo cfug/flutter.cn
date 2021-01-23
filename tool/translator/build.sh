c247f9e410debe8a4e49d6cd42edcb951e484fae
Zhicheng WANG <zcwang@thoughtworks.com>
2021-01-23 21:37:11 +0800

    fix: 去掉内存设定，现在的 nt 版本应该不需要扩大内存了。

--- tool/translator/build.sh
+++ tool/translator/build.sh
@@ -11,15 +11,13 @@ cp tool/translator/robots.txt _site
 
 cd tool/translator
 
-export NODE_OPTIONS=–max_old_space_size=8192
-
 npm i
 
 npx gulp mark-side-toc
 
-npx --node-arg --max-old-space-size=2048 nt inject '../../_site/!(community|tutorials)/**/!(*_cn).html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json
+npx nt inject '../../_site/!(community|tutorials)/**/!(*_cn).html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json
 
-npx --node-arg --max-old-space-size=2048 nt mark '../../_site/!(community|tutorials)**/!(*_cn).html'
+npx nt mark '../../_site/!(community|tutorials)**/!(*_cn).html'
 
 npx gulp remove-space
 

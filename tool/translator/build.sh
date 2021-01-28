732176a44e601a663b0eb5ec1224bbe40334fd11
Luke Cheng <2258420+chenglu@users.noreply.github.com>
2021-01-28 18:16:56 +0800

    fix: the translator tools on docs/

--- tool/translator/build.sh
+++ tool/translator/build.sh
@@ -19,6 +19,12 @@ npx nt inject '../../_site/!(community|tutorials)/**/!(*_cn).html' -c /assets/tr
 
 npx nt mark '../../_site/!(community|tutorials)/**/!(*_cn).html'
 
+# process docs/
+
+npx nt inject '../../_site/docs/*.html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json
+
+npx nt mark '../../_site/docs/*.html'
+
 #npx nt inject '../../_site/**/*.html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json
 #npx nt mark '../../_site/**/*.html'
 

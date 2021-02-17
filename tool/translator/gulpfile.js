1d5b4b582ff703067e2f76a385575c829bcc7e52
Zhicheng WANG <zcwang@thoughtworks.com>
2021-02-17 10:59:51 +0800

    fix: 处理小屏模式下的内联目录（防止误把左侧目录中连续两行的中文处理了）
    修复一个 bug

--- tool/translator/gulpfile.js
+++ tool/translator/gulpfile.js
@@ -9,6 +9,6 @@ gulp.task('remove-space', () => {
 
 gulp.task('mark-side-toc', () => {
   return gulp.src(['../../_site/**/*.html'])
-      .pipe(replace(/<li class="toc-entry (nav-item )?toc-h(\d)"><a class="nav-link" href="#(.*?)">(?!.*[\u4e00-\u9fa5])(.*?)<\/a><\/li>\n.*>(?=.*[\u4e00-\u9fa5])(.*?)<\/a>/g, '<li class="toc-entry $1toc-h$2"><a class="nav-link" href="#$3"><p>$4</p><p>$5</p></a>'))
+      .pipe(replace(/<li class="toc-entry (nav-item )?toc-h(\d)"><a (class="nav-link" )?href="#(.*?)">(?!.*[\u4e00-\u9fa5])(.*?)<\/a><\/li>\n.*>(?=.*[\u4e00-\u9fa5])(.*?)<\/a>/g, '<li class="toc-entry $1toc-h$2"><a $3href="#$4"><p>$5</p><p>$6</p></a>'))
       .pipe(gulp.dest('../../_site'));
 });

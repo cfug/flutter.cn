462a392ed189a609b16c2d61b0a9f13a57af615f
CFUG Dev Team <47591151+cfug-dev@users.noreply.github.com>
2021-06-20 12:19:05 +0800

    Update the flutter_infra bucket. (#902)

--- src/_assets/js/archive.js
+++ src/_assets/js/archive.js
@@ -7,8 +7,8 @@ var releasesToShow = 99999;
 var fetchFlutterReleases = function (os, callback, errorCallback) {
   // OS: windows, macos, linux
   // var url = "https://storage.googleapis.com/flutter_infra_release/releases/releases_" + os + ".json";
-  // TODO: CN Mirror is not ready
-  var url = "https://storage.flutter-io.cn/flutter_infra/releases/releases_" + os + ".json";
+  // TODO: CN Mirror is ready
+  var url = "https://storage.flutter-io.cn/flutter_infra_release/releases/releases_" + os + ".json";
   $.ajax({
     type: "GET",
     url: url,

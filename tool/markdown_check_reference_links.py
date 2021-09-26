637c460d354778b6e595630e4fae433784be4926
CCXXXI <ccxxxi@qq.com>
2021-09-26 19:25:56 +0800

    ci: check reference links

---
+++ tool/markdown_check_reference_links.py
@@ -0,0 +1,46 @@
+import re
+import sys
+from pathlib import Path
+from pprint import pprint
+from subprocess import check_output
+from sys import argv
+
+from tqdm import tqdm
+
+
+def check_path(path):
+    bad_files = {}
+
+    with tqdm(tuple(Path(path).rglob("*.md"))) as files:
+        for file in files:
+            file: Path
+
+            filename = file.relative_to(path).as_posix()
+
+            files.set_postfix(file=filename)
+
+            with file.open() as f:
+                html = check_output("pandoc", stdin=f, encoding="utf8")
+
+            # pre
+            html = re.sub(r"<pre.*?</pre>", "", html, flags=re.DOTALL)
+
+            # pr
+            html = re.sub(
+                r'<p><a href="https://github.com/.*?/pull/\d+">\d+</a> .*?</p>',
+                "",
+                html,
+            )
+
+            if m := re.findall(r"\[[^\[\]]+]\[[^\[\]]*]", html):
+                bad_files[filename] = m
+
+    return bad_files
+
+
+if __name__ == "__main__":
+    res = check_path(argv[1] if len(argv) == 2 else input())
+    print(f"{len(res)=}")
+    pprint(res)
+    if res:
+        sys.exit(-1)

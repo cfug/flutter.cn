5f9c2072ffefa5a92069e86176c85c91a80d05b6
CCXXXI <ccxxxi@qq.com>
2021-10-04 23:37:28 +0800

    remove tqdm
    1s查完就不用显示进度条了

--- tool/html_check_reference_links.py
+++ tool/html_check_reference_links.py
@@ -3,36 +3,32 @@ import sys
 from pathlib import Path
 from sys import argv
 
-from tqdm import tqdm
-
 
 def check_path(path):
     bad_files = {}
 
-    with tqdm(tuple(Path(path).rglob("*.html"))) as files:
-        for file in files:
-            file: Path
+    for file in Path(path).rglob("*.html"):
+        file: Path
 
-            filename = file.relative_to(path).as_posix()
-            files.set_postfix(file=filename)
+        filename = file.relative_to(path).as_posix()
 
-            with file.open(encoding="utf8") as f:
-                html = f.read()
+        with file.open(encoding="utf8") as f:
+            html = f.read()
 
-            # remove code blocks
-            html = re.sub(r"<pre.*?</pre>", "", html, flags=re.DOTALL)
+        # remove code blocks
+        html = re.sub(r"<pre.*?</pre>", "", html, flags=re.DOTALL)
 
-            # remove such lines:
-            # [foo][bar] Pull request title
-            html = re.sub(
-                r'<p><a href="https://github.com/.*?/pull/\d+">\d+</a> .*?</p>',
-                "",
-                html,
-            )
+        # remove such lines:
+        # [foo][bar] Pull request title
+        html = re.sub(
+            r'<p><a href="https://github.com/.*?/pull/\d+">\d+</a> .*?</p>',
+            "",
+            html,
+        )
 
-            # https://github.github.com/gfm/#reference-link
-            if m := re.findall(r"\[[^\[\]]+]\[[^\[\]]*]", html):
-                bad_files[filename] = m
+        # https://github.github.com/gfm/#reference-link
+        if m := re.findall(r"\[[^\[\]]+]\[[^\[\]]*]", html):
+            bad_files[filename] = m
 
     return bad_files
 

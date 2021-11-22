a8d170a23f5d6a443a43f3b1ca88842ad9d1184b
Luke Cheng <2258420+chenglu@users.noreply.github.com>
2021-11-22 06:36:00 +0000

    translation tool fixes

--- tool/translator/build.sh
+++ tool/translator/build.sh
@@ -25,6 +25,10 @@ npx nt inject '../../_site/*/index.html' -c /assets/translator/css/translator.cs
 
 npx nt mark '../../_site/*/index.html'
 
+npx nt inject '../../_site/index.html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json
+
+npx nt mark '../../_site/*/index.html'
+
 #npx nt inject '../../_site/docs/*.html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json
 #npx nt mark '../../_site/docs/*.html'
 #npx nt inject '../../_site/**/*.html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json

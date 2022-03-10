28a10a4f04b6a22192f0f4fe1b2eeae22b95221b
Alex Li <github@alexv525.com>
2022-03-10 22:30:54 +0800

    Add manipulate docs

---
+++ tool/manipulate_docs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash -e
+
+cd src
+mkdir docs
+mv -- */ docs/
+mv docs/_* ./
+mv docs/about ./
+mv docs/community ./
+mv docs/posts ./
+cp -r docs/assets ./
+rm assets/images/shared/brand
+rm docs/assets/images/shared/brand
+ln -s ../../../../site-shared/src/_assets/image assets/images/shared/brand
+ln -s ../../../../../site-shared/src/_assets/image docs/assets/images/shared/brand
+rm assets/js/vendor
+rm docs/assets/js/vendor
+ln -s ../../../site-shared/src/_assets/vendor assets/js/vendor
+ln -s ../../../../site-shared/src/_assets/vendor docs/assets/js/vendor
+cd ..

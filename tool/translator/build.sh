0b343178a3d7b48f6f93cde86738440cdf9f8e4d
Alex Li <github@alexv525.com>
2023-02-12 18:30:25 +0800

    Add listing prints

--- tool/translator/build.sh
+++ tool/translator/build.sh
@@ -11,6 +11,14 @@ cp tool/translator/robots.txt _site
 cd tool/translator
 npm i
 npx gulp mark-side-toc
+
+echo "Listing start"
+ls -al
+ls -al ../
+ls -al ../../
+ls -al ../../_site/
+echo "Listing done"
+
 npx nt inject '../../_site/!(about|community|disclaimer|posts|tutorials)/**/!(*_cn).html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json
 npx nt mark '../../_site/!(about|community|disclaimer|posts|tutorials)/**/!(*_cn).html'  --mono
 npx nt inject '../../_site/*/index.html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json

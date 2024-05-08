b1e50c751ae8bace9e44072648ffabb442eec04b
Amos <amoshuke@qq.com>
2024-05-08 16:42:34 +0800

    Update: tool/translator/build.sh

--- tool/translator/build.sh
+++ tool/translator/build.sh
@@ -6,23 +6,22 @@ cp tool/translator/robots.txt _site
 cd tool/translator
 
 npm install
+
+# 翻译前格式调整 (tool/translator/build.sh)
 npx gulp mark-side-toc
 npx gulp mark-side-level-title
 
 # !(about|community|disclaimer|posts|tutorials)/**/!(*_cn).html
 npx nt inject '../../_site/!(about|community|disclaimer|posts|tutorials)/**/!(*_cn).html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json
-npx nt export '../../_site/!(about|community|disclaimer|posts|tutorials)/**/!(*_cn).html'  --mono
+npx nt export '../../_site/!(about|community|disclaimer|posts|tutorials)/**/!(*_cn).html' --mono
 
-# community/china/!(*_cn).html
-npx nt inject '../../_site/community/china/!(*_cn).html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json
-npx nt export '../../_site/community/china/!(*_cn).html'  --mono
-
-# */index.html
-npx nt inject '../../_site/*/index.html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json
-npx nt export '../../_site/*/index.html'  --mono
+# community/china/index.html
+npx nt inject '../../_site/community/china/index.html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json
+npx nt export '../../_site/community/china/index.html' --mono
 
 # index.html
 npx nt inject '../../_site/index.html' -c /assets/translator/css/translator.css -s /assets/translator/js/translator.js -m ./url-map.json -t ./text-map.json
 npx nt export '../../_site/index.html' --mono
+
 npx gulp remove-space
 cd -

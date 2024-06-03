5577a368be83beda3576e3d6cbb3de4ca60d00d0
Amos <amoshuke@qq.com>
2024-06-03 15:01:45 +0800

    Update: compile_host_redirect_js automated pull request (#1459)

--- tool/host-redirect/build.sh
+++ tool/host-redirect/build.sh
@@ -6,11 +6,3 @@ npm run build
 
 cd -
 cp ./tool/host-redirect/build/host-redirect.min.js ./src/content/assets/js/host-redirect.min.js
-
-git init
-git config --global user.name "cfug-dev"
-git config --global user.email "cfug-dev@googlegroups.com"
-
-git add ./src/content/assets/js/host-redirect.min.js
-git commit -m "[sync] Update: host-redirect.min.js"
-git push -u -f
